3ddef9f9d62863a0116d3b15e48f3410251f3838
Andreas Böhler <dev@aboehler.at>
2015-02-04 10:43:49 +0100

    Another update for the Settings object - reload settings on each get/set call.
    Signed-off-by: Andreas Böhler <dev@aboehler.at>

--- src/com/tobykurien/batteryfu/Settings.java
+++ src/com/tobykurien/batteryfu/Settings.java
@@ -1,13 +1,8 @@
 package com.tobykurien.batteryfu;
 
-import java.util.HashMap;
-
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.widget.Toast;
 
 public class Settings {
 	public static boolean DEBUG_NIGHT_MODE = false;
@@ -21,51 +16,54 @@ public class Settings {
 	public static int FIRST_RUN_VERSION = 2;
     public static final String PREFS_NAME = "com.tobykurien.BatteryFu.shared_prefs";
 
-	private SharedPreferences mPref;
     private static Settings mInstance;
+    private Context mContext;
 
-	private Settings(SharedPreferences preferences) {
-		mPref = preferences;
-	}
+	private Settings(Context context) {
+        mContext = context.getApplicationContext();
+    }
+    
+    private synchronized SharedPreferences getPreferences()
+    {
+        SharedPreferences preferences;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+        {
+            preferences = mContext.getSharedPreferences(PREFS_NAME, Context.MODE_MULTI_PROCESS);
+        }
+        else
+        {
+            preferences = mContext.getSharedPreferences(PREFS_NAME, 0);
+        }
+        return preferences;  
+    }
 
 	public static synchronized Settings getSettings(Context context) {
         if(mInstance == null)
         {
-            SharedPreferences preferences;
-            Context ctx = context.getApplicationContext();
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-            {
-                preferences = ctx.getSharedPreferences(PREFS_NAME, 0 | Context.MODE_MULTI_PROCESS);
-            }
-            else
-            {
-                preferences = ctx.getSharedPreferences(PREFS_NAME, 0);
-            }
-            mInstance = new Settings(preferences);
+            mInstance = new Settings(context);
         }
-
       return mInstance;
 	}
 	
 	public boolean isDnsFix() {
-		return mPref.getBoolean("dns_fix", false);		
+		return getPreferences().getBoolean("dns_fix", false);		
 	}
 
    public void setIsDnsFix(boolean dnsFix) {
-      mPref.edit().putBoolean("dns_fix", dnsFix).commit();
+      getPreferences().edit().putBoolean("dns_fix", dnsFix).commit();
    }
 	
 	public boolean isEnabled() {
-		return mPref.getBoolean("enabled", false);
+        return getPreferences().getBoolean("enabled", false);
 	}
 
    public boolean isForceSync() {
-      return mPref.getBoolean("force_sync", true);
+      return getPreferences().getBoolean("force_sync", true);
    }
 	
    public boolean isNightmodeOnly() {
       if (!isNightmodeEnabled()) return false;
-      return mPref.getBoolean("night_mode_only", false);
+      return getPreferences().getBoolean("night_mode_only", false);
    }
 
    public String getNightmodeStart() {
@@ -73,160 +71,160 @@ public class Settings {
 	}
 
 	public String getNightmodeStart(String defaultVal) {
-		return mPref.getString("night_mode_start", defaultVal);
+		return getPreferences().getString("night_mode_start", defaultVal);
 	}	
 	
 	public String getNightmodeEnd() {
-		return mPref.getString("night_mode_end", Settings.DEFAULT_NIGHT_MODE_END);
+		return getPreferences().getString("night_mode_end", Settings.DEFAULT_NIGHT_MODE_END);
 	}
 	
 	public boolean isDataWhileScreenOn() {
-		return mPref.getBoolean("screen_on_data", false);
+		return getPreferences().getBoolean("screen_on_data", false);
 	}
 
 	public boolean isScreenOnKeepData() {
-      return mPref.getBoolean("screen_keep_data", true);
+      return getPreferences().getBoolean("screen_keep_data", true);
    }
 
 	public boolean isMobileDataEnabled() {
-		return mPref.getBoolean("mobile", false);
+		return getPreferences().getBoolean("mobile", false);
 	}
 
 	public boolean isDataWhileCharging() {
-		return mPref.getBoolean("charger_on_data", false);
+		return getPreferences().getBoolean("charger_on_data", false);
 	}
 	
 	public boolean isNightmodeEnabled() {
-		return mPref.getBoolean("night_mode_enabled", false);
+		return getPreferences().getBoolean("night_mode_enabled", false);
 	}
 
 	public boolean isWifiEnabled() {
-		return mPref.getBoolean("wifi", true);
+		return getPreferences().getBoolean("wifi", true);
 	}
 
 	public String getScreenOffDelayTime() {
-		return mPref.getString("screen_off_sleep_time", "30");
+		return getPreferences().getString("screen_off_sleep_time", "30");
 	}
 
 	public boolean isWaitForScreenUnlock() {
-		return mPref.getBoolean("screen_on_unlock", true);
+		return getPreferences().getBoolean("screen_on_unlock", true);
 	}
 
 	public boolean isStartOnBoot() {
-		return mPref.getBoolean("start_on_boot", true);
+		return getPreferences().getBoolean("start_on_boot", true);
 	}
 
 	public String getSleepTime() {
-		return mPref.getString("sleep_time", Settings.DEFAULT_SLEEP);
+		return getPreferences().getString("sleep_time", Settings.DEFAULT_SLEEP);
 	}
 
 	public String getAwakeTime() {
-		return mPref.getString("awake_time", Settings.DEFAULT_AWAKE);
+		return getPreferences().getString("awake_time", Settings.DEFAULT_AWAKE);
 	}
 
 	public boolean isShowNotification() {
-		return mPref.getBoolean("show_notification", true);
+		return getPreferences().getBoolean("show_notification", true);
 	}
 
 	public boolean isUseApndroid() {
-		return mPref.getBoolean("apndroid", false);
+		return getPreferences().getBoolean("apndroid", false);
 	}
 
    public boolean isFirstRun() {
-      return mPref.getInt("first_run", 0) != FIRST_RUN_VERSION;
+      return getPreferences().getInt("first_run", 0) != FIRST_RUN_VERSION;
    }
 
    public void setFirstRun() {
-      mPref.edit().putInt("first_run", FIRST_RUN_VERSION).commit();     
+      getPreferences().edit().putInt("first_run", FIRST_RUN_VERSION).commit();     
    }
    
 	public void setNightmodeStart(String start) {
-		mPref.edit().putString("night_mode_start", start).commit();		
+		getPreferences().edit().putString("night_mode_start", start).commit();		
 	}
 
 	public void setNightmodeEnd(String end) {
-		mPref.edit().putString("night_mode_end", end).commit();		
+		getPreferences().edit().putString("night_mode_end", end).commit();		
 	}
 
 	public void setEnabled(boolean enabled) {
-		mPref.edit().putBoolean("enabled", enabled).commit();
+		getPreferences().edit().putBoolean("enabled", enabled).commit();
 	}
 
 	public boolean isDataOn() {
-		return mPref.getBoolean("data_state_on", true);
+		return getPreferences().getBoolean("data_state_on", true);
 	}
 
 	public void setDataStateOn(boolean on) {
-		mPref.edit().putBoolean("data_state_on", on).commit();
+		getPreferences().edit().putBoolean("data_state_on", on).commit();
 	}
 
 	public void setSyncOnData(boolean sync) {
-		mPref.edit().putBoolean("sync_on_data", sync).commit();
+		getPreferences().edit().putBoolean("sync_on_data", sync).commit();
 	}
 
 	public boolean isSyncOnData() {
-		return mPref.getBoolean("sync_on_data", false);
+		return getPreferences().getBoolean("sync_on_data", false);
 	}
 
 	public void setIsCharging(boolean isCharging) {
-		mPref.edit().putBoolean("state_charging", isCharging).commit();
+		getPreferences().edit().putBoolean("state_charging", isCharging).commit();
 	}
 	
 	public boolean isCharging() {
-		return mPref.getBoolean("state_charging", false);
+		return getPreferences().getBoolean("state_charging", false);
 	}
 	
 	public void setIsNightmode(boolean isNightmode) {
-		mPref.edit().putBoolean("state_nightmode", isNightmode).commit();
+		getPreferences().edit().putBoolean("state_nightmode", isNightmode).commit();
 	}
 	
 	public boolean isNightmode() {
-		return mPref.getBoolean("state_nightmode", false);
+		return getPreferences().getBoolean("state_nightmode", false);
 	}
 	
 	public void setLastWakeTime(long time) {
-		mPref.edit().putLong("last_wake_time", time).commit();
+		getPreferences().edit().putLong("last_wake_time", time).commit();
 	}
 	
 	public long getLastWakeTime() {
-		return mPref.getLong("last_wake_time", 0);
+		return getPreferences().getLong("last_wake_time", 0);
 	}
 	
    public void setIsTravelMode(boolean isTravelMode) {
-      mPref.edit().putBoolean("state_travelmode", isTravelMode).commit();
+      getPreferences().edit().putBoolean("state_travelmode", isTravelMode).commit();
    }
 	
    public boolean isTravelMode() {
-      return mPref.getBoolean("state_travelmode", false);
+      return getPreferences().getBoolean("state_travelmode", false);
    }
 
    public void setUseApnDroid(boolean use) {
-      mPref.edit().putBoolean("apndroid", use).commit();
+      getPreferences().edit().putBoolean("apndroid", use).commit();
    }
    
    public boolean isDisconnectOnScreenOff() {
-      return mPref.getBoolean("state_disconnect_on_screen_off", false);
+      return getPreferences().getBoolean("state_disconnect_on_screen_off", false);
    }
    
    public void setDisconnectOnScreenOff(boolean disable) {
-      mPref.edit().putBoolean("state_disconnect_on_screen_off", disable).commit();
+      getPreferences().edit().putBoolean("state_disconnect_on_screen_off", disable).commit();
    }
 
    public void setScreenOnOff(boolean screenOnOff)
    {
-       mPref.edit().putBoolean("screen_state_on", screenOnOff).commit();
+       getPreferences().edit().putBoolean("screen_state_on", screenOnOff).commit();
    }
 
    public boolean getScreenOnOff() {
-       return mPref.getBoolean("screen_state_on", true);
+       return getPreferences().getBoolean("screen_state_on", true);
    }
 
     public long getLastRun() {
-        return mPref.getLong("last_run", 0);
+        return getPreferences().getLong("last_run", 0);
     }
 
     public void setLastRun(long run)
     {
-        mPref.edit().putLong("last_run", run).commit();
+        getPreferences().edit().putLong("last_run", run).commit();
     }
 }
