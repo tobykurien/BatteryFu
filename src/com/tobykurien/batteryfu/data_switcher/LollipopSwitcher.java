e40c58ac4b00be2463d898eaf9b5ce67ab90637c
tobykurien <toby@tobykurien.com>
2015-03-03 15:12:34 +0200

    fixed fallback to APNSwitcher when root access not acquired

--- src/com/tobykurien/batteryfu/data_switcher/LollipopSwitcher.java
+++ src/com/tobykurien/batteryfu/data_switcher/LollipopSwitcher.java
@@ -14,100 +14,106 @@ import com.tobykurien.batteryfu.R;
 
 /**
  * A Lollipop-compatible data switcher
+ * 
  * @author andy
  */
 public class LollipopSwitcher extends MobileDataSwitcher {
-    private ConnectivityManager connMan;
-    private Context context;
+	private ConnectivityManager connMan;
+	private Context context;
 
-    @Override
-    public void enableMobileData(Context context) {
+	@Override
+	public void enableMobileData(Context context) {
 
-        try {
-            // Preform su to get root priviledges
-            Process p = Runtime.getRuntime().exec("su");
+		try {
+			// Preform su to get root priviledges
+			Process p = Runtime.getRuntime().exec("su");
 
-            // Attempt to write a file to a root-only
-            DataOutputStream os = new DataOutputStream(p.getOutputStream());
-            os.writeBytes("svc data enable\n");
-            os.writeBytes("exit\n");
-            os.flush();
-            try {
-                p.waitFor();
-                if (p.exitValue() != 255) {
-                    Log.d("BatteryFu", "LollipopSwitcher: Enabled Mobile Data");
-                }
-                else {
-                    Log.d("BatteryFu", "LollipopSwitcher: Error enabling mobile data");
-                }
-            } catch (InterruptedException e) {
-                Log.d("BatteryFu", "LollipopSwitcher: Error enabling mobile data");
-            }
-        } catch (IOException e) {
-            Log.d("BatteryFu", "LollipopSwitcher: Error enabling mobile data");
-        }
+			// Attempt to write a file to a root-only
+			DataOutputStream os = new DataOutputStream(p.getOutputStream());
+			os.writeBytes("svc data enable\n");
+			os.writeBytes("exit\n");
+			os.flush();
+			try {
+				p.waitFor();
+				if (p.exitValue() != 255) {
+					Log.d("BatteryFu", "LollipopSwitcher: Enabled Mobile Data");
+				} else {
+					Log.d("BatteryFu",
+							"LollipopSwitcher: Error enabling mobile data");
+				}
+			} catch (InterruptedException e) {
+				Log.d("BatteryFu",
+						"LollipopSwitcher: Error enabling mobile data");
+			}
+		} catch (IOException e) {
+			Log.d("BatteryFu", "LollipopSwitcher: Error enabling mobile data");
+		}
 
-    }
+	}
 
-    @Override
-    public void disableMobileData(Context context) {
-        try {
-            // Preform su to get root privledges
-            Process p = Runtime.getRuntime().exec("su");
+	@Override
+	public void disableMobileData(Context context) {
+		try {
+			// Preform su to get root privledges
+			Process p = Runtime.getRuntime().exec("su");
 
-            // Attempt to write a file to a root-only
-            DataOutputStream os = new DataOutputStream(p.getOutputStream());
-            os.writeBytes("svc data disable\n");
-            os.writeBytes("exit\n");
-            os.flush();
-            try {
-                p.waitFor();
-                if (p.exitValue() != 255) {
-                    Log.d("BatteryFu", "LollipopSwitcher: Disabled Mobile Data");
-                }
-                else {
-                    Log.d("BatteryFu", "LollipopSwitcher: Error disabling mobile data");
-                }
-            } catch (InterruptedException e) {
-                Log.d("BatteryFu", "LollipopSwitcher: Error disabling mobile data");
-            }
-        } catch (IOException e) {
-            Log.d("BatteryFu", "LollipopSwitcher: Error disabling mobile data");
-        }
-    }
+			// Attempt to write a file to a root-only
+			DataOutputStream os = new DataOutputStream(p.getOutputStream());
+			os.writeBytes("svc data disable\n");
+			os.writeBytes("exit\n");
+			os.flush();
+			try {
+				p.waitFor();
+				if (p.exitValue() != 255) {
+					Log.d("BatteryFu", "LollipopSwitcher: Disabled Mobile Data");
+				} else {
+					Log.d("BatteryFu",
+							"LollipopSwitcher: Error disabling mobile data");
+				}
+			} catch (InterruptedException e) {
+				Log.d("BatteryFu",
+						"LollipopSwitcher: Error disabling mobile data");
+			}
+		} catch (IOException e) {
+			Log.d("BatteryFu", "LollipopSwitcher: Error disabling mobile data");
+		}
+	}
 
-    @Override
-    public int isToggleWorking(Context context) {
-        if(init(context))
-            return 0;
-        else
-            return R.string.need_root_text;
-    }
+	@Override
+	public int isToggleWorking(Context context) {
+		if (init(context))
+			return 0;
+		else
+			return R.string.need_root_text;
+	}
 
-    public boolean init(Context context) {
-        try {
-            // Perform su to get root priviledges
-            Process p = Runtime.getRuntime().exec("su");
+	public boolean init(Context context) {
+		try {
+			// Perform su to get root priviledges
+			Process p = Runtime.getRuntime().exec("su");
 
-            DataOutputStream os = new DataOutputStream(p.getOutputStream());
-            os.writeBytes("exit\n");
-            os.flush();
-            try {
-                p.waitFor();
-                if (p.exitValue() != 255) {
-                    Log.d("BatteryFu", "LollipopSwitcher: Successfully got root rights in init!");
-                    return true;
-                }
-                else {
-                    Log.d("BatteryFu", "LollipopSwitcher: Error gaining root access");
-                }
-            } catch (InterruptedException e) {
-                Log.d("BatteryFu", "LollipopSwitcher: Error gaining root access");
-            }
-        } catch (IOException e) {
-            Log.d("BatteryFu", "LollipopSwitcher: Error gaining root access");
-        }
-        return false;
-    }
+			DataOutputStream os = new DataOutputStream(p.getOutputStream());
+			os.writeBytes("exit\n");
+			os.flush();
+
+			try {
+				p.waitFor();
+				if (p.exitValue() == 0) {
+					Log.d("BatteryFu",
+							"LollipopSwitcher: Successfully got root rights in init!");
+					return true;
+				} else {
+					Log.d("BatteryFu",
+							"LollipopSwitcher: Error gaining root access");
+				}
+			} catch (InterruptedException e) {
+				Log.d("BatteryFu",
+						"LollipopSwitcher: Error gaining root access");
+			}
+		} catch (IOException e) {
+			Log.d("BatteryFu", "LollipopSwitcher: Error gaining root access");
+		}
+		return false;
+	}
 
 }
